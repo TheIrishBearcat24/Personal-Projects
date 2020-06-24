8878fff4db0b3bd4a0f5b8091e435e943adc8dec
TheIrishBearcat24 <61879913+TheIrishBearcat24@users.noreply.github.com>
2020-06-23 17:13:59 -0700

    First attempt at improving upon layout

--- Java/UserInterface.java
+++ Java/UserInterface.java
@@ -1,39 +1,56 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.BoxLayout;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class UserInterface {
 
+    // found these constants from playing around with the frame
+    final int maxX = 1425;
+    final int maxY = 175;
+
     JFrame frame = new JFrame("Bury the Card");
     JPanel container = new JPanel();
-    ImageGraphics rectangle1 = new ImageGraphics(0, 0, 100, 200);
-    ImageGraphics rectangle2 = new ImageGraphics(200, 100, 100, 200);
-    BoxLayout layout = new BoxLayout(container, BoxLayout.LINE_AXIS);
+    ImageGraphics rectangle1 = new ImageGraphics(50, 0, 100, 200);
+    ImageGraphics rectangle2 = new ImageGraphics(100, 0, 100, 200);
 
-    public void _frameInitialization() {
+    // BorderLayout layout = new BorderLayout(100, 100);
+
+    GridBagLayout _gbl = new GridBagLayout();
+    GridBagConstraints _gbc = new GridBagConstraints();
+
+    public void _interfaceSetup() {
         frame.setVisible(true);
-        frame.setSize(1600, 1600);
-        rectangle1.setVisible(true);
-        frame.add(rectangle1);
+        frame.setSize(1920, 1080);
 
-        container.setLayout(layout);
-        container.setVisible(true);
-
-        rectangle1.setVisible(true);
-        container.add(rectangle1);
-        rectangle1.revalidate();
-        rectangle1.repaint();
-
-        rectangle2.setVisible(true);
-        container.add(rectangle2);
-        rectangle2.revalidate();
-        rectangle2.repaint();
+        _displayRectangles();
 
         frame.add(container);
     }
 
+    public void _displayRectangles() {
+        container.setLayout(_gbl);
+        container.setVisible(true);
+
+        // rectangle2.setVisible(true);
+        // container.add(rectangle2);
+        // rectangle2.revalidate();
+        // rectangle2.repaint();
+
+        _gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        _gbc.gridx = 0;
+        _gbc.gridy = 0;
+
+        rectangle1.setVisible(true);
+        rectangle1.revalidate();
+        rectangle1.repaint();
+        container.add(rectangle1, _gbc);
+    }
+
     public static void main(String[] args) {
         UserInterface _interface = new UserInterface();
-        _interface._frameInitialization();
+        _interface._interfaceSetup();
     }
 }
