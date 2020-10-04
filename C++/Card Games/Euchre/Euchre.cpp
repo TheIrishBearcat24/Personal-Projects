f2bdcb19ba99ef7c891338f5ac2b87fe3074f02b
Cameron Clarke <clarkeck@mail.uc.edu>
2020-10-04 14:38:04 -0400

    Figured out that vector of struct is best

--- C++/Card Games/Euchre/Euchre.cpp
+++ C++/Card Games/Euchre/Euchre.cpp
@@ -1,6 +1,38 @@
 #include "Euchre.h"
 
+void Euchre::initDeck() {
+    deckOfCards = {
+        {nine, clubs},
+        {nine, hearts},
+        {nine, spades},
+        {nine, diamonds},
+        {ten, clubs},
+        {ten, hearts},
+        {ten, spades},
+        {ten, diamonds},
+        {jack, clubs},
+        {jack, hearts},
+        {jack, spades},
+        {jack, diamonds},
+        {queen, clubs},
+        {queen, hearts},
+        {queen, spades},
+        {queen, diamonds}
+    };
+}
+
+void Euchre::testPrint() {
+    for (const Deck &d : deckOfCards) {
+        cout << d.value << " of " << d.suit << endl;
+    }
+}
+
+Euchre::Euchre() {
+    initDeck();
+    testPrint();
+}
+
 int main() {
-    
+    Euchre();
     return 0;
 }
--- C++/Card Games/Euchre/Euchre.h
+++ C++/Card Games/Euchre/Euchre.h
@@ -11,50 +11,28 @@ using namespace std;
 class Euchre {
 
     private:
-        char nine = 'N';
-        char ten = 'T';
-        char jack = 'J';
-        char queen = 'Q';
-        char king = 'K';
-        char ace = 'A';
+        string nine = "Nine";
+        string ten = "Ten";
+        string jack = "Jack";
+        string queen = "Queen";
+        string king = "King";
+        string ace = "Ace";
 
         string clubs = "Clubs";
         string hearts = "Hearts";
         string spades = "Spades";
         string diamonds = "Diamonds";
 
-        map<char, string> deck = {
-            {nine, clubs},
-            {ten, clubs},
-            {jack, clubs},
-            {queen, clubs},
-            {king, clubs},
-            {ace, clubs},
-
-            {nine, hearts},
-            {ten, hearts},
-            {jack, hearts},
-            {queen, hearts},
-            {king, hearts},
-            {ace, hearts},
-            
-            {nine, spades},
-            {ten, spades},
-            {jack, spades},
-            {queen, spades},
-            {king, spades},
-            {ace, spades},
-
-            {nine, diamonds},
-            {ten, diamonds},
-            {jack, diamonds},
-            {queen, diamonds},
-            {king, diamonds},
-            {ace, diamonds}
+        struct Deck {
+            string value;
+            string suit;
         };
-    
+
+        vector<Deck> deckOfCards;
+
     public:
-        void testPrint(map<char, string> &);
+        void initDeck();
+        void testPrint();
         void shuffle();
         void dealPlayer();
         void setBauers();
