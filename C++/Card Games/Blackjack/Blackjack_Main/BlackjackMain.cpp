8e33dbb1c3ade93f5874800967df031e656894e0
Cameron Clarke <clarkeck@mail.uc.edu>
2020-10-05 19:09:44 -0400

    Deck shuffle and init up and working

--- C++/Card Games/Blackjack/Blackjack_Main/BlackjackMain.cpp
+++ C++/Card Games/Blackjack/Blackjack_Main/BlackjackMain.cpp
@@ -1,1 +1,84 @@
 #include "BlackjackMain.h"
+
+void BlackjackMain::loadDeck() {
+    deckOfCards = {
+        {two, clubs},
+        {three, clubs},
+        {four, clubs},
+        {five, clubs},
+        {six, clubs},
+        {seven, clubs},
+        {eight, clubs},
+        {nine, clubs},
+        {ten, clubs},
+        {jack, clubs},
+        {queen, clubs},
+        {king, clubs},
+        {ace, clubs},
+
+        {two, hearts},
+        {three, hearts},
+        {four, hearts},
+        {five, hearts},
+        {six, hearts},
+        {seven, hearts},
+        {eight, hearts},
+        {nine, hearts},
+        {ten, hearts},
+        {jack, hearts},
+        {queen, hearts},
+        {king, hearts},
+        {ace, hearts},
+
+        {two, spades},
+        {three, spades},
+        {four, spades},
+        {five, spades},
+        {six, spades},
+        {seven, spades},
+        {eight, spades},
+        {nine, spades},
+        {ten, spades},
+        {jack, spades},
+        {queen, spades},
+        {king, spades},
+        {ace, spades},
+
+        {two, diamonds},
+        {three, diamonds},
+        {four, diamonds},
+        {five, diamonds},
+        {six, diamonds},
+        {seven, diamonds},
+        {eight, diamonds},
+        {nine, diamonds},
+        {ten, diamonds},
+        {jack, diamonds},
+        {queen, diamonds},
+        {king, diamonds},
+        {ace, diamonds}
+    };
+}
+
+void BlackjackMain::printDeck() {
+    for (const Deck &d : deckOfCards) {
+        cout << d.ranks << " of " << d.suits << endl;
+    }
+}
+
+void BlackjackMain::shuffleDeck() {
+    srand(time(0));
+    
+    for (int i = 0; i < 3; i++) {
+        random_shuffle(deckOfCards.begin(), deckOfCards.end());
+        cout << endl;
+    }
+}
+
+int main() {
+    BlackjackMain _init;
+    _init.loadDeck();
+    _init.shuffleDeck();
+
+    return 0;
+}
--- C++/Card Games/Blackjack/Blackjack_Main/BlackjackMain.h
+++ C++/Card Games/Blackjack/Blackjack_Main/BlackjackMain.h
@@ -3,53 +3,59 @@
 
 #include <vector>
 #include <string>
-#include <variant>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <stdlib.h>
 
 using namespace std;
 
 class BlackjackMain {
     private:
-        struct suits {
-            string two = "Two";
-            string three = "Three";
-            string four = "Four";
-            string five = "Five";
-            string six = "Six";
-            string seven = "Seven";
-            string eight = "Eight";
-            string nine = "Nine";
-            string ten = "Ten";
-            string jack = "Jack";
-            string queen = "Queen";
-            string king = "King";
-            string ace = "Ace";
+        string two = "Two";
+        string three = "Three";
+        string four = "Four";
+        string five = "Five";
+        string six = "Six";
+        string seven = "Seven";
+        string eight = "Eight";
+        string nine = "Nine";
+        string ten = "Ten";
+        string jack = "Jack";
+        string queen = "Queen";
+        string king = "King";
+        string ace = "Ace";
+
+        string clubs = "Clubs";
+        string hearts = "Hearts";
+        string spades = "Spades";
+        string diamonds = "Diamonds";
+
+        // int two = 2;
+        // int three = 3;
+        // int four = 4;
+        // int five = 5;
+        // int six = 6;
+        // int seven = 7;
+        // int eight = 8;
+        // int nine = 9;
+        // int ten = 10;
+        // char jack = 10;
+        // int queen = 10;
+        // int king = 10;
+        // int ace = 11;
+
+        struct Deck {
+            string ranks;
+            string suits;
         };
 
-        struct ranks {
-            string clubs = "Clubs";
-            string hearts = "Hearts";
-            string spades = "Spades";
-            string diamonds = "Diamonds";
-        };
-
-        struct values {
-            int two = 2;
-            int three = 3;
-            int four = 4;
-            int five = 5;
-            int six = 6;
-            int seven = 7;
-            int eight = 8;
-            int nine = 9;
-            int ten = 10;
-            int jack = 10;
-            int queen = 10;
-            int king = 10;
-            int ace = 11;
-        };
+        vector<Deck> deckOfCards;
 
     public:
-    
+        void loadDeck();
+        void printDeck();
+        void shuffleDeck();
 };
 
 #endif
